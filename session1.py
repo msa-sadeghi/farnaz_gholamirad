09eb3c88a05ab1ce7fe7ada6f3ba79ea3979b445
msa-sadeghi <sadeghi.msai@gmail.com>
2024-06-23 22:00:27 +0330

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,58 @@
+import os
+os.system("cls")
+# print("hi")
+
+# x = 'aaaaaaaaaaaaaaaaaaaa'
+# print(type(x))
+
+# message = 'he said "something"'
+# print(message)
+
+# message = "he said \"something\""
+# print(message)
+
+# message = "hello\nhello\nhello"
+# print(message)
+
+# message = """hello
+# hello
+# hello"""
+# print(message)
+
+# message = '''hello
+# hello
+# hello'''
+# print(message)
+
+
+# message = '''he said "something"'''
+# print(message)
+
+# x = 1
+# print(type(x))
+# y = 1.0
+# print(type(y))
+
+# name = "roze"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print("length of string:",len(name))
+
+# a = int(input("enter a number: "))
+# print(a + 1)
+
+# +   -   /   *    //    **    %
+
+# x = int(float(input("enter a number: ")))
+# y = float(input("enter a number: "))
+
+# print(x / y)
+# print(x // y)
+
+
+x = 12
+print(type(x))
+y = str(x)
+print(type(y))
